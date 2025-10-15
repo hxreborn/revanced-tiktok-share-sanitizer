0d5ddc98aff0231e2d4856c404f846fd88df00cd
hxreborn <32096750+hxreborn@users.noreply.github.com>
2025-10-15 22:25:30 +0200

    build(gradle): add revanced patcher compilation support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 group = "app.revanced.patches.tiktok.misc"
 version = "0.1.0-SNAPSHOT"
 
+val revancedPatcherVersion = libs.findVersion("revanced-patcher").get().requiredVersion
+val revancedPatcherJar = layout.projectDirectory.file("build-env/tools/revanced-patcher-$revancedPatcherVersion-all.jar")
+
+check(revancedPatcherJar.asFile.exists()) {
+    "ReVanced patcher API JAR not found at ${revancedPatcherJar.asFile}. Run ./build-env/scripts/1-setup.sh to download it before building."
+}
+
 repositories {
     mavenCentral()
 }
@@ -12,6 +19,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(libs.okhttp)
+    compileOnly(files(revancedPatcherJar))
 
     testImplementation(kotlin("test"))
     testImplementation(libs.bundles.testing)
@@ -19,6 +27,16 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Class-Path" to configurations.runtimeClasspath.get()
+                .map { it.name }
+                .joinToString(" ")
+        )
+    }
 }
 
 kotlin {
